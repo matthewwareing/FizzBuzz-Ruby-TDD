12fc30fc70a0f882a1f7f69ae85699aa05bc7c41
Matt <mdwareing@gmail.com>
2018-04-11 12:20:55 +0100

    added irb examples and added additional tests for rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
-def fizzbuzz(n)
-  'fizz'
+def fizzbuzz n
+  answer = ''
+  answer << 'fizz' if n % 3 == 0
+  answer << 'buzz' if n % 5 == 0
+  answer == '' ? n : answer
 end
+
+# irb test
+# 101.times {|x| puts fizzbuzz x }
+# (0..100).each { |x| puts fizzbuzz x }
