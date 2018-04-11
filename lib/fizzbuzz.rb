bb58c6b11fea85da6786ecb1960ec59106ebcaa5
Matt <mdwareing@gmail.com>
2018-04-11 13:20:35 +0100

    added additional tests and added a method to the Integer class for instance testing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,6 +5,15 @@ def fizzbuzz n
   answer == '' ? n : answer
 end
 
+class Integer
+  def fizzbuzz
+    answer = ''
+    answer << 'fizz' if self % 3 == 0
+    answer << 'buzz' if self % 5 == 0
+    answer == '' ? self : answer
+  end
+end
+
 # irb test
 # 101.times {|x| puts fizzbuzz x }
 # (1..100).each { |x| puts fizzbuzz x }
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,6 @@
 require 'fizzbuzz'
 
-describe 'fizzbuzz' do 
+describe 'fizzbuzz method' do 
   it 'returns "fizz" when passed 3' do 
    expect(fizzbuzz(3)).to eq 'fizz' 
  end
@@ -13,4 +13,19 @@ describe 'fizzbuzz' do
  it 'returns 19 when passed 19' do
   expect(fizzbuzz(17)).to eq 17
  end
+end
+
+ describe "fizzbuzz instance" do
+  it 'returns "fizz" when the numbers is divisible by 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+  it 'returns "buzz" when the numbers is divisible by 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when divisible by both 3 and 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+  it 'returns 17 when neither divisible by 3 or 5' do
+    expect(17.fizzbuzz).to eq 17
+  end
 end
